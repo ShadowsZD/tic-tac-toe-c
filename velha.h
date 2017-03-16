7e622e67a515635f42088aaae9f312b3895801cb
ShadowsZD <kartkork@gmail.com>
2017-03-16 10:30:24 -0300

    Add files via upload

---
+++ velha.h
@@ -0,0 +1,13 @@
+#ifndef VELHA_H_INCLUDED
+#define VELHA_H_INCLUDED
+
+typedef struct{
+    char field[10];      //Field + /0
+    char nextSymbol;     //Next "player" to move ('X' or 'O')
+}Table;
+
+int gameEnded(Table);
+void generateNextStates(Table*, int*, Table);
+void removeParsed(Table*, int*);
+
+#endif // VELHA_H_INCLUDED
